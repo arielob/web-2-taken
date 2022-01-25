daf691118c688672994a0f5acf06843b8d87cf81
arielob <94600331+arielob@users.noreply.github.com>
2022-01-25 15:22:31 +0100

    cvv

--- kleur_popup/script.js
+++ kleur_popup/script.js
@@ -2,13 +2,10 @@ var color = prompt("give me a color");
 var el = document.getElementsByTagName("body")[0];
 console.log(el);
 el.style.backgroundColor = color;
-// dit stukje code, gaat een popup tonen, van het type "prompt"
-// die prompt gaat een vraag stellen en dan kan de gebruiker (dus diegene die website gebruikt)
-// een woord ingeven (aka string)
-var bodyColor = prompt("geef me kleur voor de body"); // we zetten woord gekregen van de gebruiker in de variabele color (zie beetje als lege doos)
-// we gaan een nieuwe variabele declareren genoemd bodyElement
-// en daarin gaan we het HTML-element steken met de naam body
+var bodyColor = prompt("geef me kleur voor de body"); 
 var bodyElement = document.getElementsByTagName("body")[0];
-// dit stuk code gaat van het body element, de style opvragen (bodyElement.style)
-// vervolgens gaat dit de backgroundColor van het body element aanpassen
 bodyElement.style.backgroundColor = bodyColor;
+
+var h1Color = prompt("geef me kleur voor de body"); 
+var h1Element = document.getElementsByTagName("h1")[0];
+h1Element.style.color = h1Color;
