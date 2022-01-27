ae313fbc830689e86a47b9b353e2615cf9bc98f0
arielob <94600331+arielob@users.noreply.github.com>
2022-01-27 16:40:00 +0100

    complete excercie

---
+++ printl-ijst/script.js
@@ -0,0 +1,10 @@
+var Arowbey = {Genre: "Hip/Rap", afkomst:"Congolees",hobbys:"sporten" };
+
+var Booba = {Genre: "Hip/Rap", afkomst:"Senegalees",hobbys:"sporten" };
+
+var Ninho = {Genre: "Hip/Rap", afkomst:"Congolees",hobbys:"Gamen" };
+
+var lijstrappers = [Arowbey, Booba, Ninho];
+for (let rappers of lijstrappers) {
+    console.log(rappers);
+}
