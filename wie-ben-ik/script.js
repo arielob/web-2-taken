330a0ef1ed6f47bc71242e3ab62d60a0290a5bf6
arielob <94600331+arielob@users.noreply.github.com>
2022-02-01 16:36:17 +0100

    update

---
+++ wie-ben-ik/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log("Ariel Owen oefening werkt");
+let h1Element = document.querySelector('.h1'); 
+console.log(h1Element);
+h1Element.style.backgroundColor = 'blue';
+
+let ariel = document.querySelector('#Ariel'); 
+ariel.style.textDecoration = "underline";
