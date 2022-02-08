5d08a04b93eb215ed867a2fa47a378f33e9c3c73
arielob <94600331+arielob@users.noreply.github.com>
2022-02-08 16:02:03 +0100

    klaar

--- wiskunde/script.js
+++ wiskunde/script.js
@@ -3,7 +3,15 @@
 let getal1 = prompt('geef het eerste getal') ;
 // Hier typt de gebruiker het cijfer dat hij wilt
 let getal2 = prompt('geef het tweede getal') ;
-
-let som = (getal1*1) + (getal2*2) ; // de som van de getallen boven
+//getal1 en getal2 zijn string geworden
+//let som = (getal1*1) + (getal2*2) ; // de som van de getallen boven
 console.log
-alert(som);
+//alert(som);
+//we moeten getal1 en getal2 vergelijken
+let isGetal1kleinerdanGetal2 = getal1 < getal2; //Dit geeftals resultaat true or false
+if(isGetal1kleinerdanGetal2) {
+    alert('Getal 1 is kleiner dan getal 2');
+} else{
+    // dit voeren we uit indien fout
+    alert('Getal 1 is groter dan getal 2');
+}
