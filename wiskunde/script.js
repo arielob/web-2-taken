4a27ea45faf82cae303561879ba385dcafa979e4
arielob <94600331+arielob@users.noreply.github.com>
2022-02-08 14:10:46 +0100

    stap 1 van wiskun de taak

---
+++ wiskunde/script.js
@@ -0,0 +1,9 @@
+//Alert gaat een popup tonen met een bericht
+//Hier zal het bericht de som zijn van 12+13
+let getal1 = prompt('geef het eerste getal') ;
+// Hier typt de gebruiker het cijfer dat hij wilt
+let getal2 = prompt('geef het tweede getal') ;
+
+let som = (getal1*1) + (getal2*2) ; // de som van de getallen boven
+console.log
+alert(som);
