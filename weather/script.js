3c16fe8b368fbdd1d21609e831d2d54a44fdaedd
arielob <94600331+arielob@users.noreply.github.com>
2022-02-17 11:16:33 +0100

    v

---
+++ weather/script.js
@@ -0,0 +1,20 @@
+const select = document.querySelector('select');
+  // const html = document.querySelector('.output');
+  const html = document.querySelector('body');
+
+  function updateBgTextColor(bgColor, textColor) {
+    html.style.backgroundColor = bgColor;
+    html.style.color = textColor;
+  }
+
+  select.onchange = function () {
+    const choice = select.value;
+
+    // ADD SWITCH STATEMENT
+    // HIERBOVEN NIETS VERANDEREN
+    // je code met switch case komt vanaf hier:
+    
+
+    // tot hier
+    // HIERONDER NIETS VERANDEREN
+  }
