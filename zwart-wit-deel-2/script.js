4d12392fdb37f8ec29103c6bff0d5be33b1fd453
arielob <94600331+arielob@users.noreply.github.com>
2022-02-22 14:47:20 +0100

    deel 2

--- zwart-wit-deel-2/script.js
+++ zwart-wit-deel-2/script.js
@@ -19,7 +19,23 @@ const selectVoorTekstKleur = document.querySelector('#tekstkleur');
   function updateBackgroundColor(achtergrondKleur) {
     html.style.backgroundColor = achtergrondKleur;
   }
-
+  selectVoorAchtergrondKleur.onchange = function () {
+  console.log('huidige waarde van de select / dropdown:', selectVoorAchtergrondKleur.value);
+    if (selectVoorAchtergrondKleur.value === 'black') {
+        updateBackgroundColor('black', 'white');
+      } else if (selectVoorAchtergrondKleur.value === 'white') {
+        updateBackgroundColor('white', 'black');
+      }
+    }
+    selectVoorTekstKleur.onchange = function () {
+      console.log('huidige waarde van de select / dropdown:', selectVoorTekstKleur.value);
+        if (selectVoorTekstKleur.value === 'black') {
+            updateTextColor('black', 'white');
+          } else if (selectVoorTekstKleur.value === 'white') {
+            updateTextColor('white', 'black');
+          }
+        }
+   
 // je moet opnieuw linken aan de juiste select (dus selectVoorAchtergrondKleur of selectVoorTekstKleur)
 // door een functie gelijk te stellen aan (naam van de select).onchange = ... (zie oefening zwart/wit of weather switch)
 
