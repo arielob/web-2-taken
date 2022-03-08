8d8c6cd3184fd3c734f8a9ead3bfc2c1fdfdeeef
arielob <94600331+arielob@users.noreply.github.com>
2022-03-08 15:03:39 +0100

    fucntie klaar

---
+++ functie/script.js
@@ -0,0 +1,15 @@
+let Arowbey = {Genre: "Hip/Rap", afkomst:"Congolees",hobbys:"sporten" };
+
+let Booba = {Genre: "Hip/Rap", afkomst:"Senegalees",hobbys:"sporten" };
+
+let Ninho = {Genre: "Hip/Rap", afkomst:"Congolees",hobbys:"Gamen" };
+let lijstrappers = [Arowbey, Booba, Ninho];
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(lijstrappers.length)
+
+for (let i = 0; i < lijstrappers.length; i++) {
+    console.log(i);
+}
