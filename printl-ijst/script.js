ab2c2ef8a46362dd2927a151a1cda35d09ae62c4
arielob <94600331+arielob@users.noreply.github.com>
2022-03-08 14:23:33 +0100

    Update script.js

--- printl-ijst/script.js
+++ printl-ijst/script.js
@@ -8,3 +8,11 @@ var lijstrappers = [Arowbey, Booba, Ninho];
 for (let rappers of lijstrappers) {
     console.log(rappers);
 }
+
+const namen = ["Dirk", "Sandy", "Tormund", "Dahlia"];
+
+for (let i = 0; i < namen.length; i++) {
+    // Gebruik de teller i als index van de array
+    // Elke iteratie wordt er dus een nieuw element aangesproken
+    console.log(namen[i]);
+}
